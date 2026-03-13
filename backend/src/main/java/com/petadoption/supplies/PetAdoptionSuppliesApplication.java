8449d522ae2ce1335ab557934b015d4e32ba9aa5
Dharshan20055 <dharshanmurali05@gmail.com>
2026-03-13 14:05:56 +0530

    folders added

---
+++ backend/src/main/java/com/petadoption/supplies/PetAdoptionSuppliesApplication.java
@@ -0,0 +1,13 @@
+package com.petadoption.supplies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetAdoptionSuppliesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetAdoptionSuppliesApplication.class, args);
+	}
+
+}
